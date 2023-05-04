a1a032e4396488bf698d01b5f4f8cc2388747dbd
anavas95 <alejandrolopez9544@gmail.com>
2023-05-04 11:26:21 -0600

    temperatura

--- Codigos de ejemplo/Sensor_de_Temperatura/Sensor_de_Temperatura.ino
+++ Codigos de ejemplo/Sensor_de_Temperatura/Sensor_de_Temperatura.ino
@@ -7,36 +7,41 @@
  * Taller de electronica digital y reparacion de computadoras
  * Nombre: Profe Alejandro Lopez
  * Fecha: 27/04/2023
- * Proyecto: Sensor de Temperatura DS18B20
+ * Proyecto: LCD por medio de I2C
  */
 
+
 //Librerias
-#include <OneWire.h>                
-#include <DallasTemperature.h>
- 
+#include <OneWire.h>            //Libreria para usar el protocolo de comunicacion OneWire
+#include <DallasTemperature.h>  //Libreria para usar el sensor de temperatura DS18B20
+
 //Constructores
-OneWire ourWire(2);                //Se establece el pin 2  como el pin que utilizaremos para el protocolo OneWire
-DallasTemperature sensors(&ourWire); //Se declara una objeto para nuestro sensor
+
+OneWire com_OneWire(2);     //Especifico en que pin utilizare para comunicarme con el sensor de temperatura
+DallasTemperature Sensor_Temperatura(&com_OneWire);  //Especifico que el sensor de temperatura se encontrara conectado al pin donde utilizo la comunicacion OneWire
 
 
+#include <Wire.h>
+#include "PCF8574.h"
 void setup() {
-delay(1000);
-Serial.begin(9600);   //Inicio la comunicacion serial
-sensors.begin();   //Se inicia el sensor
+  Serial.begin(9600); //Inicio la comunicacion serial
+  Sensor_Temperatura.begin();  //inicio la comunicacion hacia el sensor
 }
- 
+
 void loop() {
-sensors.requestTemperatures();   //Se envía el comando para leer la temperatura
-float temp= sensors.getTempCByIndex(0); //Se obtiene la temperatura en ºC
-float temp1 = sensor.getTempFByIndex(0); //Se obtiene la temperatura en ºF
 
-Serial.print("Temperatura= ");
-Serial.print(temp);
-Serial.println(" C");
-delay(100);  
-Serial.print("Temperatura=");
-Serial.print(temp1);
-Serial.println("ºF);
-delay(100);
+  Sensor_Temperatura.requestTemperatures();  //solicito al sensor la temperatura medida
+
+  float Temperatura_C = Sensor_Temperatura.getTempCByIndex(0);  //Obtengo la temperatura en grados centigrados
+  float Temperatura_F = Sensor_Temperatura.getTempFByIndex(0);  //Obtengo la temperatura en grados farenheit
+
+
+  //Impresion de la temperatura
+  Serial.print("La temperatura en grados centigrados es de: ");
+  Serial.println(Temperatura_C);
+  Serial.print("La temperatura en grados farenheit es de: ");
+  Serial.println(Temperatura_F);
+
+  delay(500); //espera de 500ms
+ 
 }
-
