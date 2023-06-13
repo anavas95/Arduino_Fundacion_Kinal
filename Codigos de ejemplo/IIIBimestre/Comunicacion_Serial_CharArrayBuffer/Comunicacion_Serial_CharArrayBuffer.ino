773ff423cb8601bcfea29f699280a0aa83d4e52a
Alejandro Navas <alejandrolopez9544@gmail.com>
2023-06-13 11:17:34 -0600

    Comunicación Serial
    Recepción on de una cadena de caracteres

--- Codigos de ejemplo/IIIBimestre/Comunicacion_Serial_CharArrayBuffer/Comunicacion_Serial_CharArrayBuffer.ino
+++ Codigos de ejemplo/IIIBimestre/Comunicacion_Serial_CharArrayBuffer/Comunicacion_Serial_CharArrayBuffer.ino
@@ -7,43 +7,56 @@
  * Curso: Taller de electronica digital y reparación de computadoras I
  * Nombre: Profe Alejandro
  * Carnet: ---------
- * Proyecto: Comunicacion serial - recepcion de un solo caracter
+ * Proyecto: Comunicacion serial - recepcion de una cadena de caracteres
 */
+#include <stdlib.h>
+#include <stdio.h>
 
-
-//***************MACROS*************************
-#define led 13 //led conectado al pin 2 del arduino.
+//***********************MACRO**********************************
+#define led 13   //led conectado al pin 13 del arduino
 #define SerialBaudRate 9600 //velocidad de la comunicacion serial
 
 
-void setup() 
+//***********************VARIABLE*******************************
+
+/*
+Definimos un char array, en el cual podemos almacenar hasta un maximo de 20 bytes.
+*/
+char inf_recibida[20];  //20 espacios de memoria para almacenar datos
+
+void setup()
 {
- Serial.begin(9600);
- pinMode(led, OUTPUT);
- digitalWrite(led, LOW);
- Serial.println("Clase de comunicacion serial - recepcion de un unico caracter");
+  Serial.begin(SerialBaudRate); //Inicializo la comunicacion serial a una velocidad de 9600 baudios.  
+  pinMode(led, OUTPUT); //Declaro el pin 2 como salida
+  pinMode(led, LOW);
+  Serial.println("Clase de comunicacion serial - recepcion de una cadena de caracteres");
 }
 
 void loop() 
 {
-  //Mientras tenga caracteres disponibles para leer en los pines de comunicacion serial
-  while(Serial.available()>0)
+
+  if(Serial.available()>0) //verificamos que hayan datos en el buffer serial
   {
-    //Guardo el caracter recibido por medio de la comunicacion serial
-    char caracter_recibido = Serial.read();
+     /*
+     El operador size_t determina el tamaño de un objeto (Es decir el array tiene 20 elementos, size_t devuelve 20)
+     Esto unicamente lo realizo para saber cuantos caracteres he recibido por medio de la comunicacion serial
+     */
+     //size_t nombre_cualquiera = Serial.readBytesUntil(caracter_delimitador, array que utilizo como buffer, el tamanio del buffer)
 
-    if(caracter_recibido == 'A')
+     size_t datos = Serial.readBytesUntil('\r\n', inf_recibida, 20);
+    Serial.print("El caracter recibido es: ");  //Imprimimos el texto en el monitor serial
+    
+    for(int i=0;i<datos;i++)                 //Recorro el buffer para almacenar dentro de el la informacion recibida
     {
-      //Si he recibido la letra mayuscula A
-      digitalWrite(led, HIGH); //enciendo el led
+      inf_recibida[i];
     }
-    else if(caracter_recibido == 'B')
-    {
-     digitalWrite(led, LOW); 
-    }
+    Serial.println(inf_recibida);  //envio al monitor serial la informacion almacenado en mi char buffer
 
-    //La siguiente linea es unicamente para ver el caracter recibido
-    Serial.print("El caracter recibido es: ");
-    Serial.println(caracter_recibido);
-  }
-}
+    //Limpio el charArray, es decir borro la informacion contenida dentro de el
+    for(int i = 0; i<datos; i++)
+    {
+      //cada posicion del charArray se iguala NULL
+      inf_recibida[i] = '\0';
+    }
+ }
+} 
